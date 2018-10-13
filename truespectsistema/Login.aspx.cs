9e171adc73cedb048412270ae5751880061c3071
oaperezb <oaperezb@cotemar.com.mx>
2018-10-12 19:42:32 -0500

    Ajax implementation

--- truespectsistema/Login.aspx.cs
+++ truespectsistema/Login.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -13,25 +14,25 @@ namespace truespectsistema
         {
 
         }
-        protected void Submit_Click(object sender, EventArgs e)
+        //protected void Submit_Click(object sender, EventArgs e)
+        [WebMethod]
+        public static void Submit_Click(string usuario, string password)
         {
             TruespectEntities db = new TruespectEntities();
             var consulta = (from q in db.usuarios
-                            where q.usuario == txtusuario.Value && q.contraseña == txtcontraseña.Value
+                            where q.usuario == usuario && q.contraseña == password
                             select q).SingleOrDefault();
-
             if (consulta != null)
             {
-                Session["usuario"] = consulta.usuario;
-                Session["nombre"]=consulta.nombres+" "+consulta.apellidos;
-                Response.Redirect("Inicio.aspx");
-
+                HttpContext.Current.Session["usuario"] = consulta.usuario;
+                HttpContext.Current.Session["nombre"] = consulta.nombres + " " + consulta.apellidos;
+                //HttpContext.Current.Response.Redirect("Inicio.aspx");
             }
-            else
-            {
-                string validarusuario = "alert('El usuario o la Contraseña es Incorrecta');";
-                ScriptManager.RegisterStartupScript(this, typeof(Page), "alerta", validarusuario, true);
-            }
+            //else
+            //{
+            //    string validarusuario = "alert('El usuario o la Contraseña es Incorrecta');";
+            //    ScriptManager.RegisterStartupScript(this, typeof(Page), "alerta", validarusuario, true);
+            //}
         }
     }
 }
